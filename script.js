e67d1df7bbb93216e8a7fcd33ee0bfa08e48bd17
Kris <kelleyblackmore@gmail.com>
2016-10-11 17:24:41 +0000

    added an alert to the button

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.getElementById("foot01").innerHTML = "<p>&copy;  " + new Date().getFullYear() + " Kris Kelley. All rights reserved.</p>";
+
+function warnonAlert(){
+    alert("This isn't working yet");
+    
+}
