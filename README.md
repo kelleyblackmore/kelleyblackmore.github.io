ba6f8e4e041941b713ec8c3b88e4121c49dfa1fc
Kris <kelleyblackmore@gmail.com>
2017-03-15 16:54:35 +0000

    adding js navbar

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ This is my projects website.
 Note:
 <ul>
 <li>Favicon</li>
-<li>content for about me</li>
+<li></li>
 
 <li> better layout </li>
 
 </ul>
 ![Marshtomp](http://randompokemon.com/sprites/animated/350.gif)
+
--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function warnonAlert(){
     
     
 }
+function addNavbar(){
+    
+    
+    document.getElementById('navbar').innerHTML = 
+/**Navbar */
+'<div class="w3-top">'+
+ ' <ul class="w3-navbar w3-blue w3-card-2 w3-left-align w3-large">'+
+   ' <li class="w3-hide-medium w3-hide-large w3-opennav w3-right">'+
+    '</li>'+
+    '<li><a href="index.html" class="w3-padding-large w3-white">Home</a></li>'+
+    '<li class="w3-hide-small"><a href="projects.html" class="w3-padding-large w3-hover-white">Projects</a></li>'+
+   '<li class="w3-hide-small"><a href="blog.html" class="w3-padding-large w3-hover-white">Blog</a></li>'+
+    '<li class="w3-hide-small"><a href="learntocode.html" class="w3-padding-large w3-hover-white">Learn to code</a></li>'+
+    '<li class="w3-hide-small"><a href="contactme.html" class="w3-padding-large w3-hover-white">Contact me</a></li>'+
+  '</ul>';
+  
+  }
