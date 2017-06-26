c8085f06af85570d29439683a68313e18011e4b7
Kris Kelley <Kris Kelley>
2017-06-25 19:16:34 -0700

    testing nav in js

--- script.js
+++ script.js
@@ -9,24 +9,46 @@ function warnonAlert(){
 function init(){
     
     addNavbar();
-    addFooter();
-    getFiles();
+   // addFooter();
+  //  getFiles();
 }
 function addNavbar() {
     
 document.getElementById('navbar').innerHTML =
-   '<div>'+
- ' <ul >'+
+'<nav class="navbar navbar-default" >'+
+    '<div class="container-fluid">'+
+        '<div class="navbar-header">'+
+        '<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsedNav">'+
+        '<span class="sr-only">Toggle navigation</span>'+    
+        '<span class="icon-bar"></span>'+
+        '<span class="icon-bar"></span>'+
+         '<span class="icon-bar"></span>'+   
+        '</button>'+
+        '<a href="#" class="navbar-brand"><img src="photos/favicon.ico" style="height:100%; width: auto;" alt="Kelleyblackmore.github.io"/></a>'+
+        '<p class="navbar-text"><strong>Kris=Dev</strong></p>'+
+        '</div>'+
+        '<div class="collapse navbar-collapse" id="collapseNav">'+
+            '<ul class="nav navbar-nav">'+
+            '<li class="active"><a href="#">Home</a></li>'+
+            '<li><a href="aboutme.html">About Me</a></li>'+
+            '<li><a  href="blog.html">Blog</a></li>'+
+            '<li><a  href="projects.html">Projects</a></li>'+
+            '<li><a  href="learntocode.html">Learn to code</a></li>'+
+            '<li><a  href="projects.html">Projects</a></li>'+
+            '</ul>'+    
+            '<form class="navbar-form navbar-right" role="search">'+
+            '<div class="form-group">'+
+                '<input type="text" class="form-control" placeholder="Search">'+
+            '</div>'+
+            '</form>'+
+            '<ul class="nav navbar-nav navbar-right">'+
+            '<li><a href="">Contact me</a></li>'+
+            '</ul>'+
+        '</div>'+
+    '</div>'+
+    '</nav>';
 
-    '<li><a href="index.html">Home</a></li>'+
-    '<li><a href="blog.html" >Blog</a></li>'+
-    '<li ><a href="projects.html" >Projects</a></li>'+
-    '<li ><a href="games.html" >Games</a></li>'+
-    '<li><a href="certificates.html" >Certificates</a></li>'+
-    '<li ><a href="learntocode.html" >Learn to code</a></li>'+
-    '<li><a href="tribute.html" >Tribute Page </a></li>'+
-    '<li ><a href="contactme.html" >Contact me</a></li>'+
-  '</ul>';
+
 }
 
 function addFooter(){
