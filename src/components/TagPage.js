cddb3fe08516592a08257695b38880ed92b3c4ec
Kris <kelleyblackmore@gmail.com>
2025-12-25 07:53:03 +0000

    Updates

--- src/components/TagPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Blog from './Blog';
-
-export default function TagPage() {
-  const { tag } = useParams();
-  return <Blog initialTag={tag} />;
-}
